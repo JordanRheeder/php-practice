4f386222a139acdab8db52b0d94cf3897e29fc59
jordanrheeder <jordanrheeder@gmail.com>
2019-10-02 18:14:40 +0200

    basic echo hello world

---
+++ test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Hi :)</title>
+    </head>
+    <body>
+        <?php
+            echo "test 123 \n123    "
+         ?>
+    </body>
+</html>
